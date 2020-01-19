d25f1e9c6c1a2265f2241f9b7dff8136c13f29ea
Domogo <domsatrapa@gmail.com>
2020-01-19 17:29:09 +0100

    mob scene - enemies

--- objects/Player.gd
+++ objects/Player.gd
@@ -1,10 +1,13 @@
 extends Area2D
 
+signal hit
+
 export var speed = 400 # speed of player (pixels per sec)
 var screen_size
 
 func _ready():
 	screen_size = get_viewport_rect().size
+	hide() # hide the player when the game starts
 
 func _process(delta):
 	var velocity = Vector2() # the player's movement vector
@@ -18,9 +21,27 @@ func _process(delta):
 		velocity.y -= 1
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
+		if velocity.x != 0:
+			$AnimatedSprite.animation = "right"
+			$AnimatedSprite.flip_v = false
+			$AnimatedSprite.flip_h = velocity.x < 0 # if < 0 flip h, else don't
+		elif velocity.y != 0:
+			$AnimatedSprite.animation = "up"
+			$AnimatedSprite.flip_v = velocity.y > 0
 		$AnimatedSprite.play()
 	else:
 		$AnimatedSprite.stop()
 	position += velocity * delta
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
+
+
+func _on_Player_body_entered(body):
+	hide() # player disappear after being hit
+	emit_signal("hit")
+	$CollisionShape2D.set_deferred("disabled", true)
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
