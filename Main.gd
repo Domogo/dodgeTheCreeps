741c120eb94ab0ec306e95738adbfe18c245e428
Domogo <domsatrapa@gmail.com>
2020-01-19 17:58:49 +0100

    Main scene, spawning enemies, counting score

---
+++ Main.gd
@@ -0,0 +1,45 @@
+extends Node
+
+export ( PackedScene ) var Mob
+var score
+
+func _ready():
+	randomize()
+
+
+func game_over():
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+
+
+func new_game():
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+
+
+func _on_MobTimer_timeout():
+	# choose random location in path2d
+	$MobPath/MobSpawnLocation.set_offset(randi())
+	# create a mob instance and add to scene
+	var mob = Mob.instance()
+	add_child(mob)
+	# set mob direction perpendicular to the path direction
+	var direction = $MobPath/MobSpawnLocation.rotation + PI / 2
+	# set the mob position to a random location
+	mob.position = $MobPath/MobSpawnLocation.position
+	# add some randomness to the direction
+	direction += rand_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+	# set the velocity ( speed and direction )
+	mob.linear_velocity = Vector2(rand_range(mob.min_speed, mob.max_speed), 0)
+	mob.linear_velocity = mob.linear_velocity.rotated(direction)
+
+
+func _on_ScoreTimer_timeout():
+	score += 1
+
+
+func _on_StartTimer_timeout():
+	$MobTimer.start()
+	$ScoreTimer.start()
